6014c028ba4f1e9ffb1bfa9c79cea5a78723a21c
IgorPszczola <251208@edu.p.lodz.pl>
2026-06-12 20:54:12 +0200

    Add project files.

---
+++ HorizonTravel/Model/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace HorizonTravel.Model
+{
+    public class Role
+    {
+        public int id { get; set; }
+        public string nazwaRoli { get; set; } = string.Empty;
+
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
