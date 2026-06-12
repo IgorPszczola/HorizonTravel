e4de261bfd2764309eed08514ab81124c4f30ea9
IgorPszczola <251208@edu.p.lodz.pl>
2026-06-12 22:20:17 +0200

    add Models

--- HorizonTravel/Model/Role.cs
+++ HorizonTravel/Model/Role.cs
@@ -1,9 +1,9 @@
-﻿namespace HorizonTravel.Model
+namespace HorizonTravel.Model
 {
     public class Role
     {
-        public int id { get; set; }
-        public string nazwaRoli { get; set; } = string.Empty;
+        public int Id { get; set; }
+        public string NazwaRoli { get; set; } = string.Empty;
 
         public virtual ICollection<User> Users { get; set; } = new List<User>();
     }
